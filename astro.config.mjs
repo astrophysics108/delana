82109474e7e5c315240c0ad18c8288f76fa88fb0
Astrophysics108 <dsingh@perse.co.uk>
2025-08-15 15:00:06 +0100

    setup github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://astrophysics108.github.io',
+  base: 'delana',
+})
+
