4db0fae1da950ece8380885c06085a9b13df7835
Astrophysics108 <dsingh@perse.co.uk>
2025-08-21 19:55:59 +0100

    clean up start making stars

---
+++ src/scripts/star.js
@@ -0,0 +1,12 @@
+function createStar(x, y){
+    document.innerHTML += ""
+}
+
+
+var x = 0;
+var y = 0;
+for (i in range(100), i++) {
+    x = Math.random()*100;
+    y = Math.random()*100;
+    createStar(x,y)
+}
